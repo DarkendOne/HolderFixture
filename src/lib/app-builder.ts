54fb10eb3ebf26e52efbfeb4200340ee1c32423b
DarkendOne <claytad@gmail.com>
2026-06-14 15:25:19 -0400

    Updateing Schema

--- src/lib/app-builder.ts
+++ src/lib/app-builder.ts
@@ -1,29 +1,7 @@
 import * as THREE from 'three';
 import { ThreeViewer } from './viewer';
 import { exportSTL, downloadSTL, getTriangleCount } from './exporter';
-
-export interface BaseParamSchema {
-  id: string;
-  label: string;
-  type: string;
-  showIf?: (params: Record<string, any>) => boolean;
-}
-
-export interface RangeParamSchema extends BaseParamSchema {
-  type: 'range';
-  min?: number;
-  max?: number;
-  step?: number;
-  default: number;
-  unit?: string;
-}
-
-export interface CheckboxParamSchema extends BaseParamSchema {
-  type: 'checkbox';
-  default: boolean;
-}
-
-export type ParamSchema = RangeParamSchema | CheckboxParamSchema;
+import { ParamSchema, RangeParamSchema, CheckboxParamSchema } from './schema';
 
 export interface AppConfig {
   title: string;
